2a47bc62622aa091daa5066a11a33ef02c79c8e3
skdevop397 <skdevop.23@gmail.com>
2026-05-30 18:42:39 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "RG2345" {
   name     = "RG-sadashiv"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "RG235" {
+  name     = "RG-sadashiv1"
+  location = "West Europe"
+}
